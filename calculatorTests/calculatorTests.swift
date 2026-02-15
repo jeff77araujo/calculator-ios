5de8d7048c265213b57e65ae2b847f9d49316c2d
Jeff Araujo <jefferson_lord@yahoo.com.br>
2026-02-15 17:39:40 -0300

    Initial Commit

---
+++ calculatorTests/calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by Jefferson Oliveira de Araujo on 15/02/26.
+//
+
+import Testing
+@testable import calculator
+
+struct calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
